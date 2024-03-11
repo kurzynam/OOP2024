cd28a162504419891753fe7e8132e1d423cb59f3
mkurzyna <kurzynama@office.umcs.pl>
2024-03-11 23:29:31 +0100

    Lab 3

--- first_project/src/SvgScene.java
+++ first_project/src/SvgScene.java
@@ -11,6 +11,12 @@ public class SvgScene {
         shapes.add(shape);
     }
 
+    public ArrayList<Shape> getShapes() {
+        return shapes;
+    }
+    public void setShapes(ArrayList<Shape> shapes) {
+        this.shapes = shapes;
+    }
     public SvgScene() {
         shapes = new ArrayList<>();
     }
@@ -35,66 +41,4 @@ public class SvgScene {
         code += "</svg>";
         return code;
     }
-
-//    private ArrayList<Shape> shapes;
-//    private int width, height;
-//
-//    public int getWidth() {
-//        return width;
-//    }
-//
-//    public void setWidth(int width) {
-//        this.width = width;
-//    }
-//
-//    public int getHeight() {
-//        return height;
-//    }
-//
-//    public void setHeight(int height) {
-//        this.height = height;
-//    }
-//
-//    public SvgScene(ArrayList<Shape> shapes, int width, int height) {
-//        this.shapes = shapes;
-//        this.width = width;
-//        this.height = height;
-//    }
-//    public SvgScene(int width, int height) {
-//        this.shapes = new ArrayList<>();
-//        this.width = width;
-//        this.height = height;
-//    }
-//
-//    public ArrayList<Shape> getShapes() {
-//        return shapes;
-//    }
-//
-//    public void setShapes(ArrayList<Shape> shapes) {
-//        this.shapes = shapes;
-//    }
-//
-//    public void addShape(Shape p){
-//        shapes.add(p);
-//    }
-//
-//    public String toSvg(){
-//        String code = "<svg height=\" " + getHeight() + " \" width=\"   " + getWidth() + "\" xmlns=\"http://www.w3.org/2000/svg\">";
-//
-//        for (Shape shape : shapes){
-//            code += shape.toSvg();
-//        }
-//        code += "</svg>";
-//        return code;
-//    }
-//    public void writeToFile(String filePath) {
-//        try {
-//            BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
-//            writer.write(toSvg());
-//            writer.close();
-//            System.out.println("Plik " + filePath + " został poprawnie zapisany!");
-//        } catch (IOException e) {
-//            System.err.println("Wystąpił błąd podczas zapisywania do pliku: " + e.getMessage());
-//        }
-//    }
 }
