81c889254a1f2138023916ee28aa64b78758b3b4
mkurzyna <kurzynama@office.umcs.pl>
2024-03-11 23:26:34 +0100

    Lab 3

--- first_project/src/Point.java
+++ first_project/src/Point.java
@@ -1,5 +1,3 @@
-import java.lang.Math;
-
 class Point {
     public int getX() {
         return x;
--- first_project/src/Polygon.java
+++ first_project/src/Polygon.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Polygon extends Shape{
     private ArrayList<Point> points;
